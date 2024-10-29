fad9786f7a4089b615d4264173fcd928fd6b3757
Bhoopendra Singh <bsyadav2002.apj@gmail.com>
2024-10-29 11:42:43 +0530

    Create a react app with VITE

---
+++ 01vitereact/src/Greeting.jsx
@@ -0,0 +1,9 @@
+function Greeting() {
+  return (
+    <>
+      <h3>Namaskāram, EveryOne 🙏 </h3>
+    </>
+  );
+}
+
+export default Greeting;
---
+++ 01vitereact/src/Order_chai.jsx
@@ -0,0 +1,15 @@
+function Chai() {
+  return (
+    <>
+      <h1>Ordering Chai</h1>
+      <h4>About</h4>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse quis iste
+        sit, hic unde illum. Necessitatibus, cum! Quisquam, iure fuga. Dolores
+        praesentium ipsam sunt quidem mollitia ipsa quaerat voluptas id.
+      </p>
+    </>
+  );
+}
+
+export default Chai;
